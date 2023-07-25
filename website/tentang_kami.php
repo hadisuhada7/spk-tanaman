1096498996647b1cba90d47781522aa4777ae895
Hadi Suhada <140533690+hadisuhada7@users.noreply.github.com>
2023-07-25 23:33:37 +0700

    Add files via upload

---
+++ website/tentang_kami.php
@@ -0,0 +1,109 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+	<head>
+		<title> Tentang Kami | Diskusi Petani </title>
+		<meta charset='utf-8'>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel='shortcut icon' href='images/fav-icon.jpg'/>
+		<link rel='stylesheet' type='text/css' href='css/style.css'/>
+		<link rel='stylesheet' type='text/css' href='css/js-image-slider.css'/>
+		<link rel='stylesheet' type='text/css' href='css/slider.css'/>
+		<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+		<script src="js/script.js"></script>
+		<script src="js/js-image-slider.js" type="text/javascript"></script>
+	<body>
+		<div id='header-wrap'>
+			<div id='header'>
+				<div id='logo'>
+		            <a href="beranda.php">
+		            	<img src="images/logo.png"/>
+		            </a>
+		            <div class="sosial-media">
+		                <a href="https://www.facebook.com" target="_blank">
+		                   	<img src="images/sosmed/facebook.png" width="40" height="40" border="0"> 
+		                </a>
+		                <a href="https://www.twitter.com" target="_blank">
+		                    <img src="images/sosmed/twitter.png" width="40" height="40" border="0"> 
+		                </a>
+		                <a href="https://www.instagram.com" target="_blank">
+		                    <img src="images/sosmed/instagram.png" width="40" height="40" border="0"> 
+		                </a>
+		                <a href="https://www.youtube.com" target="_blank">
+		                    <img src="images/sosmed/youtube.png" width="40" height="40" border="0"> 
+		                </a>
+		                <a href="kontak_kami.php">
+		                    <img src="images/sosmed/call_us_now.png" width="130" height="40" border="0"> 
+		                </a>
+		            </div>
+		        </div>
+			</div>
+		</div>
+		<div id='menu-wrap'>
+			<div id='menu-padding'>
+				<div id='cssmenu'>
+					<ul>
+						<li><a href="beranda.php"><b>Beranda</b></a></li>
+						<li class='has-sub'><a href="tentang_kami.php"><b>Tentang Kami</b></a>
+							<ul>
+								<li><a href="profil.php">Profil</a>
+								<li><a href="visi_misi.php">Visi & Misi</a>
+								<li><a href="penghargaan.php">Penghargaan</a>
+							</ul>
+						</li>
+						<li><a href="publikasi.php"><b>Publikasi</b></a></li>
+						<li><a href="informasi_publik.php"><b>Informasi Publik</b></a></li>
+						<li><a href="berita.php"><b>Berita</b></a></li>
+						<li><a href="kontak_kami.php"><b>Kontak Kami</b></a></li>
+						<li><a href="//localhost/spk-tanaman/login.php" target="_blank"><b>Login</b></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id='main-wrap'>
+			<div id='main-center'>
+				<div id='head-main'>
+					<span> Tentang Kami </span>
+				</div>
+				<div id='main'>
+				
+				<table class="bordered">
+					<thead>
+						<tr>
+							<th> Tentang Kami </th>
+						</tr>
+						<tr>
+							<td>
+								<a href="profil.php"> Profil </a>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<a href="visi_misi.php">Visi & Misi</a>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<a href="penghargaan.php">Penghargaan</a>
+							</td>
+						</tr>
+					</thead>
+				</table>
+				
+				</div>
+			</div>
+		</div>
+		
+		<div id='footer'>
+			<div id='footer-wrap'>
+				<div class="cleaner_h20"></div>
+					<div align="center">
+						Copyright &copy; <?=date('Y')?> Sistem Pendukung Keputusan Pemilihan Jenis Tanaman Pangan <br>
+						All Rights Reserved.
+					</div>
+				<div class="cleaner_h30"></div>
+			</div>
+		</div>	
+	</body>
+</html>
